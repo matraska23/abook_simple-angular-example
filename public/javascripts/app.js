171ce95ab31a07da6471bbd3d2e876728c8bd518
matraska123 <nsmalcev@bk.ru>
2014-08-07 20:18:20 +0400

    add angular controller

---
+++ public/javascripts/app.js
@@ -0,0 +1,45 @@
+﻿
+function BookController($scope){
+    $scope.query = "";
+	
+	var sesId = 'aa';
+	var counter = 0;
+	
+	$scope.book = [{
+		name: 'aaa',
+		tel: '123',
+		id: 'aa_33'
+	},{
+		name: 'Геном Гермофродитович Дарвинский',
+		tel: '1234',
+		id: 'aa_34'
+	},{
+		name: 'ccc',
+		tel: '12345',
+		id: 'aa_34'
+	}].map(function(item){
+		item.activeClass = "";
+		return item;
+	});
+	
+	$scope.addNew = function(){
+		$scope.book.push({
+			name: Math.random(),
+			tel: Math.random(),
+			id: 'aa_' + counter++
+		});	
+	};
+	$scope.editSelected = function(){
+		console.log('Edit');
+	};
+	$scope.removeSelected = function(){
+		console.log('Delete');
+	};
+	$scope.select = function(item){
+		if(item.activeClass == '__selected'){
+			item.activeClass = '';
+		}else{
+			item.activeClass = '__selected'
+		}
+	};
+}
