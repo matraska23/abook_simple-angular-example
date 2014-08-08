5634a67b992df56c436ce76ecb2100660c88e36f
matraska123 <nsmalcev@bk.ru>
2014-08-08 16:35:44 +0400

    add request

--- app.js
+++ app.js
@@ -20,6 +20,111 @@ router.get('/', function(req, res){
 	});
 });
 
+var _helpers = {
+	arrayToHash: function(list){
+		var res = {};
+		for(var i = 0, len = list.length; i < len; i++){
+			res[list[i]] = i;
+		}
+		return res;
+	}
+};
+
+var _contactModel = {
+	list: [{
+		name: 'aaa',
+		tel: '123',
+		id: 'aa_33'
+	},{
+		name: 'Геном Гермофродитович Дарвинский',
+		tel: '1234',
+		id: 'aa_34'
+	},{
+		name: 'ccc',
+		tel: '12345',
+		id: 'aa_35'
+	}],
+	init: function(){
+		// todo
+	},
+	store: function(){
+		// todo
+	},
+	count: 0,
+	remove: function(idList){
+		var 	idHash = _helpers.arrayToHash(idList),
+				removeCount = 0;
+	
+		for(var i = this.list.length - 1; i >= 0; i--){
+			if(idHash[this.list[i].id] != undefined){
+				this.list.splice(i, 1);
+				removeCount++;
+			}
+		}
+		return removeCount;
+	},
+	add: function(name, tel){
+		var id = ++this.count;
+		
+		this.list.push({
+			id: id,
+			name: name,
+			tel: tel
+		});
+		
+		return id;
+	},
+	update: function(id, name, tel){
+		var updated = 0;
+		for(var i = 0, len = this.list.length; i < len; i++){
+			if(this.list[i].id == id){
+				if(name){
+					this.list[i].name = name;
+				}
+				if(tel != undefined){
+					this.list[i].tel = tel;
+				}
+				updated++;
+				break;
+			}
+		}
+		return updated;
+	}
+};	
+	
+
+	
+router.get('/load', function(req, res) {
+	res.json({
+		contacts: _contactModel.list
+	});
+});
+
+router.post('/add', function(req, res) {
+	var id = _contactModel.add(req.body.name, req.body.tel);
+	
+	res.json({
+		id: id
+	});
+});
+
+router.post('/delete', function(req, res) {
+	var removeCount = _contactModel.remove(req.body.id);
+		
+	res.json({
+		removed: removeCount
+	});
+});
+
+router.post('/update', function(req, res) {
+	var updateCount = _contactModel.update(req.body.id, req.body.name, req.body.tel);
+		
+	res.json({
+		updated: updateCount
+	});
+});
+
+
 app.use('/', router);
 
 /// catch 404 and forward to error handler
@@ -28,6 +133,8 @@ app.use(function(req, res, next) {
     err.status = 404;
     next(err);
 });
+
+
 
 /// error handlers
 
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,55 +1,144 @@
-﻿
-function BookController($scope){
+﻿var abookApp = angular.module('abook', []);
+
+abookApp.filter('contactSearch', function(){
+	return function(item){
+		console.log('From filter');
+		console.dir(item);
+		return item;
+	};
+});
+
+abookApp.factory('dataSource', function($http){
+	return {
+		getContactList: function(onReady){
+			$http({
+				method: 'GET', 
+				url: '/load/'
+			}).
+				success(function(data, status, headers, config) {
+					onReady(data);
+				}).
+				error(function(data, status, headers, config) {
+					console.log(status);
+				});
+		},
+		addContact: function(conf, onReady){
+			$http({
+				method: 'POST', 
+				url: '/add/',
+				data: conf
+			}).
+				success(function(data, status, headers, config) {
+					onReady(data);
+				}).
+				error(function(data, status, headers, config) {
+					console.log(status);
+				});
+		},
+		deleteContacts: function(idList, onReady){
+			$http({
+				method: 'POST', 
+				url: '/delete/',
+				data: {
+					id: idList
+				}
+			}).
+				success(function(data, status, headers, config) {
+					onReady(data);
+				}).
+				error(function(data, status, headers, config) {
+					console.log(status);
+				});
+		
+		},
+		updateContact:  function(conf, onReady){
+			$http({
+				method: 'POST', 
+				url: '/update/',
+				data: conf
+			}).
+				success(function(data, status, headers, config) {
+					onReady(data);
+				}).
+				error(function(data, status, headers, config) {
+					console.log(status);
+				});
+		
+		}
+	};
+});
+
+abookApp.controller('BookController', function BookController($scope, dataSource){
     $scope.query = "";
 	
-	var sesId = 'aa';
-	var counter = 0;
 	var _SEL_CLASS = '__selected';
 	
-	$scope.book = [{
-		name: 'aaa',
-		tel: '123',
-		id: 'aa_33'
-	},{
-		name: 'Геном Гермофродитович Дарвинский',
-		tel: '1234',
-		id: 'aa_34'
-	},{
-		name: 'ccc',
-		tel: '12345',
-		id: 'aa_34'
-	}].map(function(item){
-		item.activeClass = "";
-		return item;
+	dataSource.getContactList(function(d){
+		$scope.book = d.contacts.map(function(item){
+			item.activeClass = "";
+			return item;
+		});
 	});
 	
 	$scope.addNew = function(){
-		$scope.book.push({
+		var contactData = {
 			name: Math.random(),
 			tel: Math.random(),
-			id: 'aa_' + counter++
-		});	
+		};
+		
+		dataSource.addContact(contactData, function(d){
+			contactData.id = d.id;
+			$scope.book.push(contactData);	
+		});
 	};
 	$scope.editSelected = function(){
 		console.log('Edit');
+		var conf = {
+			name: Math.random(),
+			tel: Math.random(),
+		};
 		
 		for(var i = 0, len = $scope.book.length; i < len; i++){
 			if($scope.book[i].activeClass == _SEL_CLASS){
-				$scope.book[i].name = Math.random();
-				$scope.book[i].tel = Math.random();
+				conf.id = $scope.book[i].id;
 				break;
 			}
 		}
 		
+		if(!conf.id){
+			return;
+		}
+		
+		dataSource.updateContact(conf, function(d){
+			$scope.book[i].name = conf.name;
+			$scope.book[i].tel = conf.tel;
+		});
+		
 	};
 	$scope.removeSelected = function(){
 		console.log('Delete');
+		var 	idList = [],
+				idHash = {};
 				
 		for(var i = $scope.book.length - 1; i >= 0; i--){
 			if($scope.book[i].activeClass == _SEL_CLASS){
-				$scope.book.splice(i, 1);
+				//$scope.book.splice(i, 1);
+				idList.push($scope.book[i].id);
+				idHash[$scope.book[i].id] = i;
 			}
 		}
+		
+		if(!idList.length){
+			return;
+		}
+		
+		dataSource.deleteContacts(idList, function(d){
+			for(var i = $scope.book.length - 1; i >= 0; i--){
+				if(idHash[$scope.book[i].id] != undefined){
+					$scope.book.splice(i, 1);
+				}
+			}
+		});
 	};
 	$scope.select = function(item){
 		if(item.activeClass == _SEL_CLASS){
@@ -58,4 +147,5 @@ function BookController($scope){
 			item.activeClass = _SEL_CLASS;
 		}
 	};
-}
+
+});
