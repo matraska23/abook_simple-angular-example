da1dbef6d800ae997e107920f4b4a8960a08935c
matraska123 <nsmalcev@bk.ru>
2014-08-15 18:47:46 +0400

    add dialogs

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -122,14 +122,16 @@ abookApp.controller('popups', ['$scope', '$element', 'popupBus', function ($scop
 
 // Popup wrapper (get content from controller)
 abookApp.directive('abookPopupWrap', function ($compile) {
+//	`content` and `config` attributes would be shared to child directives!
 	return {
 		restrict: 'E',
 		transclude: true,
 		scope: {
 			'close': '&onClose',
-			'content': '=content'
+			'content': '=content',
+			'config': '=config',
 		},
-		template: '<div ><div ng-click="close()">[X]</div>{{content}}</div><div data-co="inner"></div>',
+		template: '<div ><div ng-click="close()">[X]</div></div><div data-co="inner"></div>',
 		link: function (scope, elem, attr){
 			var contentSource = $compile(scope.content)(scope);
 			var inner = elem[0].querySelector('[data-co=inner]');
@@ -137,24 +139,74 @@ abookApp.directive('abookPopupWrap', function ($compile) {
 			if(inner && contentSource[0]){
 				inner.appendChild(contentSource[0]);
 			}
-			console.log('Popup scope');
-			console.dir(scope);
 			// also can listen on `elem`
-			scope.$on('$destroy', function(){
-				// ...
-			});
+			// scope.$on('$destroy', function(){
+				// // ...
+			// });
 		}
 	}
 });
 
-
-/*
-.directive('myCustomer', function() {
-    return {
-      template: 'Name: {{customer.name}} Address: {{customer.address}}'
-    };
-  });
-*/
+abookApp.directive('editDialog', function($compile){
+	return {
+		restrict: 'EA',
+		transclude: true,
+		scope: {
+			
+		},
+		template: 
+			'<div class="">' +
+				'<form ng-submit="onsubmit($event)">' +
+					'<div class="">Name*: <input type="text" class="" required data-co="name" value="{{name}}"/></div>' +
+					'<div class="">Tel: <input type="text" class="" data-co="tel" value="{{tel}}"/></div>' +
+					'<div class=""><button type="submit">{{button}}</button></div>' +
+				'</form>' +
+			'</div>',
+		link: function ($scope, $elem, attr){
+			var config = $scope.$parent.config; 
+			// console.log('Edit dialog');
+			// console.dir(config);
+			// console.dir($elem);
+			// window.d = $elem;
+			// console.dir($elem.find('[type=submit]'));
+			
+			if(config.button){
+				$scope.button = config.button;
+			}
+			
+			if(config.name){
+				$scope.name = config.name;
+			}
+			
+			if(config.tel){
+				$scope.tel = config.tel;
+			}
+			$scope.onsubmit = function(e){
+				// console.log('HANDLE SUBMIT');
+				// console.dir($scope.$parent);
+				
+				if(config.onsubmit){
+					var 	nameInput = $4.fnd($elem[0], '[data-co=name]'),
+							telInput = $4.fnd($elem[0], '[data-co=tel]'),
+							name = nameInput && nameInput.value,
+							tel = telInput && telInput.value;
+							
+					config.onsubmit(name, tel);
+				}
+				
+				// TODO:
+				// I don't know how to trigger event with bubbling on the parents
+				// $scope.$parent.$parent.$parent.$emit('close');
+				
+				// use internal api to close popup:
+				var popupData = $scope.$parent.$parent.popup;
+				$scope.$parent.$parent.closePopup(popupData);
+				
+				e.preventDefault();
+			};
+		}
+	}
+});
 
 abookApp.controller('BookController', ['$scope', 'dataSource', 'popupBus', function BookController($scope, dataSource, $popups){
     $scope.query = "";
@@ -169,45 +221,65 @@ abookApp.controller('BookController', ['$scope', 'dataSource', 'popupBus', funct
 	});
 	
 	$scope.addNew = function(){
-		var contactData = {
-			name: Math.random(),
-			tel: Math.random(),
-		};
-		
-		dataSource.addContact(contactData, function(d){
-			contactData.id = d.id;
-			$scope.book.push(contactData);	
-			
-			$popups.add({
-				onopen: function(){},
-				onclose: function(){},
-				content: '<div>[AAA]</div>'
-			});
+		$popups.add({
+			content: 
+				'<div>' +
+					'<edit-dialog ></edit-dialog>' +
+				'</div>',
+			config: {
+				button: 'Add',
+				onsubmit: function(name, tel){
+				
+					var contactData = {
+						name: name,
+						tel: tel,
+					};
+					
+					dataSource.addContact(contactData, function(d){
+						contactData.id = d.id;
+						$scope.book.push(contactData);	
+					});
+				},
+			}
 		});
 	};
 	$scope.editSelected = function(){
-		console.log('Edit');
-		var conf = {
-			name: Math.random(),
-			tel: Math.random(),
-		};
+		var editId;
 		
 		for(var i = 0, len = $scope.book.length; i < len; i++){
 			if($scope.book[i].activeClass == _SEL_CLASS){
-				conf.id = $scope.book[i].id;
+				editId = $scope.book[i].id;
 				break;
 			}
 		}
 		
-		if(!conf.id){
+		if(!editId){
 			return;
 		}
 		
-		dataSource.updateContact(conf, function(d){
-			$scope.book[i].name = conf.name;
-			$scope.book[i].tel = conf.tel;
+		$popups.add({
+			content: 
+				'<div>' +
+					'<edit-dialog ></edit-dialog>' +
+				'</div>',
+			config: {
+				button: 'Change',
+				name: $scope.book[i].name,
+				tel: $scope.book[i].tel,
+				onsubmit: function(name, tel){
+					var contactData = {
+						name: name,
+						tel: tel,
+						id: editId
+					};
+				
+					dataSource.updateContact(contactData, function(d){
+						$scope.book[i].name = contactData.name;
+						$scope.book[i].tel = contactData.tel;
+					});
+				},
+			}
 		});
-		
 	};
 	$scope.removeSelected = function(){
 		console.log('Delete');
